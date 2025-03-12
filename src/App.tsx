17599f39abb4830e6ed68f4ebc316c106db31061
GautamAggarwalDev <gautam.aggarwal.tech@gmail.com>
2025-03-12 16:28:40 +0530

    add feedback page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 // import TellUsAbout from "./components/TellUsAbout.tsx";
-
-import OrderTable from "./components/OrderTable.tsx"
+// import OrderTable from "./components/OrderTable.tsx"
+import FeedbackPage from "./components/FeedbackPage.tsx"
 
 function App() {
 
   return (
     <>
       {/* <TellUsAbout /> */}
-      <OrderTable />
+      {/* <OrderTable /> */}
+      <FeedbackPage />
     </>
   )
 }
